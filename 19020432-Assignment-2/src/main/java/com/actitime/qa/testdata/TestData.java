87d74f44dc5624d2216daffdf5ff757aea1b044d
Akila Maithripala <46641264+akilamaithri@users.noreply.github.com>
2023-11-06 09:51:02 +0530

    Update TestData.java
    Second commit

--- 19020432-Assignment-2/src/main/java/com/actitime/qa/testdata/TestData.java
+++ 19020432-Assignment-2/src/main/java/com/actitime/qa/testdata/TestData.java
@@ -6,9 +6,9 @@ public class TestData extends TestBase {
     public TestData() {
         super();
     }
-    public static final String FIRST_NAME = "Claire";
-    public static final String MIDDLE_NAME = "EL";
-    public static final String LAST_NAME= "Cottrill";
-    public static final String EMAIL = "clairecotroll@gmail.com";
-    public static final String DEPARTMENT = "Quality Control";
+    public static final String FIRST_NAME = "Akila";
+    public static final String MIDDLE_NAME = "Deshan";
+    public static final String LAST_NAME= "Maithripala";
+    public static final String EMAIL = "akilamaithripala@gmail.com";
+    public static final String DEPARTMENT = "QC";
 }
